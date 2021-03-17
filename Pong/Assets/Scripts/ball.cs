dbe87ed23049a5b26d66e06aa4b2b973aed55e9d
Noctis112 <ruimartins96_@hotmail.com>
2021-03-17 21:48:36 +0000

    Pong
    ainda por acabar

---
+++ Pong/Assets/Scripts/ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+    [SerializeField]
+    float lancarBola = 2f;
+
+    float tempo = 0.0f;
+    bool bolaJaFoiLancada = false;
+
+    void Movimento()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitSphere;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        tempo += Time.deltaTime; //tempo = tempo + Time.deltaTime;
+
+        //if (bolaJaFoiLancada == false && tempo > lancarBola)
+        //{
+        //    Movimento();
+        //    bolaJaFoiLancada = true;
+        //}
+        if (bolaJaFoiLancada == false)
+        {
+            if (tempo > lancarBola)
+            {
+                Movimento();
+                bolaJaFoiLancada = true;
+            }
+
+        }
+    }
+
+}
---
+++ Pong/Assets/Scripts/paddle.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+    [SerializeField]
+    float velocity = 5f;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            //transform.position =
+            // transform.position + velocity * Vector3.up * Time.deltaTime;
+            transform.position =
+                transform.position += velocity * Vector3.up * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(downKey))
+        {
+            transform.position =
+                 transform.position + velocity * Vector3.down * Time.deltaTime;
+        }
+
+        float cameraHeight = Camera.main.orthographicSize;
+        float halfPaddleSize = 0.5f;
+
+        //if (transform.position.y >= cameraHeight - halfPaddleSize)
+        //{
+        //    Vector3 positionAux = transform.position;
+        //    positionAux.y = cameraHeight - halfPaddleSize;
+        //    transform.position = positionAux;
+        //}
+        //if (transform.position.y <= -cameraHeight - halfPaddleSize)
+        //{
+        //    Vector3 positionAux = transform.position;
+        //    positionAux.y = -cameraHeight + halfPaddleSize;
+        //    transform.position = positionAux;
+        //}
+
+        Vector3 positionAux = transform.position;
+        positionAux.y = Mathf.Clamp(transform.position.y,
+            -cameraHeight + halfPaddleSize,
+            cameraHeight - halfPaddleSize);
+        transform.position = positionAux;
+
+        //codigo para ir para baixo
+        //if (transform.position.y >= cameraHeight + halfPaddleSize)
+        //{
+        //    Vector3 positionAux = transform.position;
+        //    positionAux.y = -cameraHeight - halfPaddleSize;
+        //    transform.position = positionAux;
+        //}
+    }
+
+}
